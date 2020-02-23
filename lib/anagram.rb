5814c4c0680813217a66f0e514cfaedc290964b7
Kyle Talmage <ktalmage85@gmail.com>
2020-02-23 15:12:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,60 @@
-# Your code goes here!
+
+
+require 'pry'
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+    
+  end
+    
+  def match(words_array)
+    words_array.select do |item|
+      @word.split(" ").sort == item.split(" ").sort
+      binding.pry
+      end
+      words_array
+  end 
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
