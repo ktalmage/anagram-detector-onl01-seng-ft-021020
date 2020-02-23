32efbf6c01bc16d11c81b4e97080c9b264168deb
Kyle Talmage <ktalmage85@gmail.com>
2020-02-23 15:32:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,15 +8,16 @@ class Anagram
   
   def initialize(word)
     @word = word
-    
   end
- def match(array_of_words)   
+  
+  def match(array_of_words)   
   return_array = []
     array_of_words.each do |element|
       (@word.split("").sort) == (element.split("").sort)
         return_array << element
       end
     end
+  end
     return_array
     
     array_of_words.select do |element|
