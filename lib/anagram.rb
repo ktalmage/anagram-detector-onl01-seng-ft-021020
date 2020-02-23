839e5f111c15fceb28bae5b632d525cc2aea568a
Kyle Talmage <ktalmage85@gmail.com>
2020-02-23 15:37:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
 class Anagram
-  attr_accessor :word_to_analyze
+  attr_accessor :word
   
-  def initialize(word_to_analyze)
-    @word_to_analyze = word_to_analyze 
+  def initialize(word)
+    @word = word 
   end 
   
   def match(find_anagram_in_this_string) 
-    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    puts "Must find #{self.word} inside the following string: #{find_anagram_in_this_string}"
     find_anagram_in_this_string.find_all do |word| 
-      if word.split("").sort == self.word_to_analyze.split("").sort 
+      if word.split("").sort == self.word.split("").sort 
         word 
       end
     end 
